e1d9563a607f1ac06eb4b9b25dfca0e801399d12
ferauche <thiago.ferauche@gmail.com>
2020-09-10 19:59:37 -0300

    Aula5

---
+++ Aulas/src/main/java/br/unisantos/br/aula5/Aula_5.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unisantos.br.aula5;
+
+/**
+ *
+ * @author ferauche
+ */
+public class Aula_5 {
+    
+    public static void main(String args[]) {
+        
+        OperadorCaixa op = new OperadorCaixa("Thiago F","8712631927391-X",1500.00, 1234, 1);
+        Segurança seg = new Segurança("Joao", "123123131231", 900.00, 1111, "Portão principal");
+        Gerente gerente = new Gerente("Ciro C. T.","231231231-X",5000, 4321,1000);
+        
+        //System.out.println(func.getNome()+" "+logar(func, 2222));
+        System.out.println(gerente.getNome()+" "+logar(gerente, 4321));
+        System.out.println(op.getNome()+" "+logar(op, 4321));
+        System.out.println(seg.getNome()+" "+logar(seg, 4321));
+        
+        gerente.cancelarOperacao();
+        op.registrar();
+        seg.acionarAlarme();
+    }
+    
+    private static boolean logar(Funcionario f, int senha) {
+        return f.autenticar(senha);
+    }
+    
+}
---
+++ Aulas/src/main/java/br/unisantos/br/aula5/OperadorCaixa.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unisantos.br.aula5;
+
+/**
+ *
+ * @author ferauche
+ */
+public class OperadorCaixa extends Funcionario {
+    
+    private int numeroCaixa;
+    
+    public OperadorCaixa(String nome, String cpf, double sal, int senha, int numCx){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = sal;
+        this.senha = senha;
+        this.numeroCaixa = numCx;
+    }
+    
+    public void registrar() {
+        System.out.println("Produto registrado");
+    }
+
+    public int getNumeroCaixa() {
+        return numeroCaixa;
+    }
+
+    public void setNumeroCaixa(int numeroCaixa) {
+        this.numeroCaixa = numeroCaixa;
+    }
+    
+    
+    
+}
