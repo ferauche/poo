23f1cebcbcf63346acc9b19fd3df483880395526
ferauche <thiago.ferauche@gmail.com>
2020-11-12 15:42:45 -0300

    Proposta de atividade aula13 para utilizar Collection Set

---
+++ Aulas/src/main/java/br/unisantos/aula13/Candidato.java
@@ -0,0 +1,51 @@
+package br.unisantos.aula13;
+
+/**
+ *
+ * @author ferauche
+ */
+public class Candidato implements Comparable<Candidato> {
+    
+    private String nome;
+    private int numero;
+    private int totalVotos;
+    
+    public Candidato() {
+        
+    }
+    
+    public Candidato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    public void setTotalVotos(int t) {
+        this.totalVotos = t;
+    }
+    
+    public int getTotalVotos() {
+        return this.totalVotos;
+    }
+
+    @Override
+    public int compareTo(Candidato o) {
+        return o.getTotalVotos() - this.totalVotos;
+    }
+    
+}
